1a3cb29134c4657f278ba50aa2eb0e049674f7b6
jupiter9381 <jupiter9381@gmail.com>
2020-06-30 18:16:41 +1000

    db added

---
+++ lead_contact.sql
@@ -0,0 +1,36 @@
+/*
+SQLyog Community v13.1.3  (64 bit)
+MySQL - 10.4.11-MariaDB : Database - crm
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`crm` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `crm`;
+
+/*Table structure for table `lead_contacts` */
+
+DROP TABLE IF EXISTS `lead_contacts`;
+
+CREATE TABLE `lead_contacts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `contact_date` datetime DEFAULT NULL,
+  `lead_id` int(11) DEFAULT NULL,
+  `agent_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
